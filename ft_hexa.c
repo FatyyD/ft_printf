e4d19899ab02c1c28bd5e924c987a94f1af8571c
FatyyD <79169041+FatyyD@users.noreply.github.com>
2022-03-11 19:21:46 +0100

    Update ft_hexa.c

--- ft_hexa.c
+++ ft_hexa.c
@@ -2,7 +2,6 @@
 
 void    ft_conver(int n, char *base, int *count)
 {
-  int i;
     if (n < 0)
 	{
 		ft_putchar('-', count);
@@ -18,24 +17,41 @@ void    ft_conver(int n, char *base, int *count)
     {
         ft_putchar(n + '0', count);
     }
-    else if (format[i] == 'X')
+    else
     {
         ft_putchar(n - 10 + 'a', count);
     }
+}
+
+void    ft_convermaj(int n, char *base, int *count)
+{
+    if (n < 0)
+	{
+		ft_putchar('-', count);
+		ft_convermaj(n* (-1), base, count);
+	}
+   else if (n >= 16)
+    {
+       ft_convermaj(n / 16, base, count);
+       ft_convermaj(n % 16, base, count);
+       
+    }
+    else if (n >= 0 && n <= 9)
+    {
+        ft_putchar(n + '0', count);
+    }
     else
     {
-        ft_putchar(n - 10 - 'A', count);
+        ft_putchar(n - 10 + 'A', count);
     }
 }
-
-int    ft_hexa( const char *format, int *count)
+/*int    ft_hexa( const char *format, int *count)
 {
-    int i;
-    unsigned int k;
+    unsigned int i;
 
-        if (format[k] == 'x')
+        if (*format == 'x')
             ft_conver(i, "0123456789abcdef", count);
-        if (format[k] == 'X')
+        if (*format == 'X')
             ft_conver(i, "0123456789ABCDEF", count);
      return (0);
-}
+}*/
